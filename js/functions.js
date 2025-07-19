1d274e89306292fa7106dce303bf9703cbe2452d
Demeward <did.dod1520@mail.ru>
2025-07-19 12:49:41 +0300

    Добавляет функцию на проверку времени встречи

--- js/functions.js
+++ js/functions.js
@@ -42,3 +42,52 @@ const getIntegerNumber = function (param) {
 };
 
 getIntegerNumber('1 кефир, 0.5 батона');
+
+const checkMeetingTime = function (workStart, workEnd, meetingTime, meetingDuration) {
+
+  //Добавляет ноль в случае, если время указано одной цифрой
+  const addZeroToString = function (string) {
+    const splittedStr = string.split(':');
+    if (splittedStr[0].length === 1) {
+      splittedStr[0] = `0${splittedStr[0]}`;
+    }
+    if (splittedStr[1].length === 1) {
+      splittedStr[1] = `0${splittedStr[1]}`;
+    }
+
+    return splittedStr.join(':');
+  };
+
+  // Если рабочий день не укладывается в одни календарные сутки
+  if (addZeroToString(workStart) >= addZeroToString(workEnd)) {
+    return false;
+  }
+
+  let meetingTimes = meetingTime.split(':');
+
+  const meetingMinutes = (parseInt(meetingTimes[1], 10)) + meetingDuration;
+
+  if (meetingMinutes >= 60) {
+    const hours = Math.floor(meetingMinutes / 60);
+    const minutes = meetingMinutes % 60;
+    meetingTimes[0] = (parseInt(meetingTimes[0], 10) + hours).toString();
+    meetingTimes[1] = minutes.toString();
+  } else {
+    meetingTimes[1] = meetingMinutes.toString();
+  }
+
+  // Если встреча не укладывается в одни календарные сутки
+  if (meetingTimes[0] > 23) {
+    return false;
+  }
+
+  meetingTimes = meetingTimes.join(':');
+
+  return (addZeroToString(meetingTimes) >= addZeroToString(workStart) && addZeroToString(meetingTimes) <= addZeroToString(workEnd));
+};
+
+checkMeetingTime('08:00', '17:30', '14:00', 90);
+checkMeetingTime('8:0', '10:0', '8:0', 120);
+checkMeetingTime('08:00', '14:30', '14:00', 90);
+checkMeetingTime('14:00', '17:30', '08:0', 90);
+checkMeetingTime('8:00', '17:30', '08:00', 900);

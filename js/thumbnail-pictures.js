799738c91c8493d20e6e92fd3c2d782eb27a10ce
Demeward <did.dod1520@mail.ru>
2025-07-30 07:21:39 +0300

    Добавляет полноэкранный режим изображений

--- js/thumbnail-pictures.js
+++ js/thumbnail-pictures.js
@@ -1,8 +1,10 @@
+import { openBigPicture } from './big-picture';
+
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesContainer = document.querySelector('.pictures');
 const picturesContainerFragment = document.createDocumentFragment();
 
-const renderPictures = function (pictures) {
+const renderPictures = (pictures) => {
   pictures.forEach(({ url, description, likes, comments }) => {
     const pictureElement = pictureTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = url;
@@ -10,6 +12,11 @@ const renderPictures = function (pictures) {
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comments.length;
     picturesContainerFragment.appendChild(pictureElement);
+
+    pictureElement.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      openBigPicture({url, description, likes, comments});
+    });
   });
 
   picturesContainer.appendChild(picturesContainerFragment);
